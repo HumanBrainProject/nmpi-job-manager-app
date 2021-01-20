94af9832e8e3f7913976bfb2d3738995790b9b5b
hassen aguili <hassen.aguili@etudiant-enit.utm.tn>
2021-01-20 03:20:47 +0100

    syntax highlight

--- src/Queue/JobDetail.js
+++ src/Queue/JobDetail.js
@@ -4,10 +4,26 @@ import {
   useParams
 } from "react-router-dom";
 //import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import * as Mui from '@material-ui/core';
+import Prism from "prismjs";
+import hljs from 'highlight.js';
 // import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 // import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '100%',
+    textAlign: "left",
+    //backgroundColor: theme.palette.background.paper,
+  },
+  boxStyle: {
+
+  textAlign: "left",
+
+  },
+}));
 function JobDetail(props) {
   let { id } = useParams();
   const[job, setJob] = useState({});
@@ -28,33 +44,35 @@ function JobDetail(props) {
     };
     fetchData();
   }, []);
-
+  const classes = useStyles();
   return(
     <div>
       <h2>Job {id}</h2>
-          <div>
-            <p>
-              <span className={job.status === 'finished' ? 'badge badge-success' : 'badge badge-danger'}>{job.status}</span>
-            </p>
-            <p>
-              <small class="ng-binding">
-                Submitted on {job.timestamp_submission} by {job.user_id} to <strong class="ng-binding">{job.hardware_platform}</strong>
-              </small>
-              <br></br>
-              <small class="ng-binding">
-                Completed on {job.timestamp_completion}
-              </small>
-            </p>
+        <div>
+        <p>
+          <span className={job.status === 'finished' ? 'badge badge-success' : 'badge badge-danger'}>{job.status}</span>
+        </p>
+        <p>
+          <small class="ng-binding">
+            Submitted on {job.timestamp_submission} by {job.user_id} to <strong class="ng-binding">{job.hardware_platform}</strong>
+          </small>
+          <br></br>
+          <small class="ng-binding">
+            ompleted on {job.timestamp_completion}
+          </small>
+        </p>
 
 
-            <Mui.Box component="span" boxShadow={3} display="block">Code</Mui.Box>
-            
+
+            <Mui.Button component="span"className={classes.root}   variant="contained" color="primary">Code</Mui.Button>
+            <Mui.Box component="span"className={classes.boxStyle}  boxShadow={0} display="block">
             <pre>
-            <code>
+            <code  class="Python">
             
             {job.code}
             </code>
           </pre>
+          </Mui.Box>
             <Mui.Box component="span" boxShadow={3} display="block">Command</Mui.Box>
             <pre>
             <code>
@@ -83,8 +101,19 @@ function JobDetail(props) {
       </code>
     </pre>
 
+    <Mui.Grid container  spacing={2}>
+    <Mui.Grid item xs={12}>
+      <Mui.Grid container justify="center" >
+        {[0, 1, 2].map((value) => (
+          <Mui.Grid key={value} item>
+            <Mui.Paper  />
+          </Mui.Grid>
+        ))}
+      </Mui.Grid>
+    </Mui.Grid>
+    </Mui.Grid>
 
-          </div>
+      </div>
     </div>
   );
 }
