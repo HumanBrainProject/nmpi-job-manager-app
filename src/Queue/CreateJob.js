884a22fab8942bbdc8df6eec3f9e622a2c96a14d
msenoville <matthieu.senoville@unic.cnrs-gif.fr>
2021-01-26 14:48:29 +0100

    before moving to mui

--- src/Queue/CreateJob.js
+++ src/Queue/CreateJob.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+// import React from "react";
+import Tabs from "./../Components/Tabs";
+
 
 const hw_options = [
   {
@@ -38,7 +41,9 @@ class CreateJob extends Component {
     render() {
       return (
         <div id="App">
+
           <h5>Hardware Platform</h5>
+
           <div className="select-container">
             <select onChange={this.handleChange}>
             <option disabled selected value> -- Please choose a simulation platform -- </option>
@@ -47,7 +52,56 @@ class CreateJob extends Component {
               ))}
             </select>
           </div>
+
+          <br/>
+          
           <h5>Code</h5>
+
+            <Tabs>
+              <div label="Editor">
+                to do
+              </div>
+              <div label="From Git repository or zip archive">
+                to do 
+              </div>
+              <div label="From the Drive">
+                to do
+              </div>
+            </Tabs>
+
+          <br/>
+          
+          <h5>Command</h5>
+          
+            <form>
+                <input type="text" name="command" />
+            </form>
+
+          <br/>
+          
+          <h5>Hardware Config</h5>
+
+            <form>
+                <input type="text" name="config" />
+            </form>
+
+          <br/>
+          
+          <h5>Tags</h5>
+
+            <form>
+                <input type="text" name="tag" />
+            </form>
+
+          <br/>
+          
+          <h5>Input Files</h5>
+
+            <form>
+                <legend>Enter temperature in :</legend>
+                <input type="text" name="input" />
+            </form>
+
         </div>
       );
     }
