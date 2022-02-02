2b12924cd5de2620f55fcb5808970f106adc238f
Hassen Aguili <75277274+Haguili@users.noreply.github.com>
2022-02-02 16:27:39 +0100

    fteaures related to joblist : Pagination + filters + sort by ID column + a lot of styling changes

---
+++ src/Utils.js
@@ -0,0 +1,35 @@
+
+
+
+export function timeFormat(defaultFormatDate)
+{
+let formattedDate = String(defaultFormatDate).slice(0,4)+"/"+String(defaultFormatDate).slice(5,7)+"/"+String(defaultFormatDate).slice(8,10)+" "+String(defaultFormatDate).slice(11,19);
+
+return formattedDate;
+
+}
+
+
+export function currentDate()
+{let currentdate = new Date();
+  return  currentdate.getDate() + "/"
+    + (currentdate.getMonth()+1)  + "/"
+    + currentdate.getFullYear() + " @ "
+    + currentdate.getHours() + ":"
+    + currentdate.getMinutes() + ":"
+    + currentdate.getSeconds();
+
+}
+
+
+
+export function currentDateFileFormat()
+{let currentdate = new Date();
+  return  currentdate.getDate() + "_"
+    + (currentdate.getMonth()+1)  + "_"
+    + currentdate.getFullYear() + "_"
+    + currentdate.getHours() + "_"
+    + currentdate.getMinutes() + "_"
+    + currentdate.getSeconds();
+
+}
