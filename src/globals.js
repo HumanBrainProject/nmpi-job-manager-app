5eec4b43876004ad9f2b18650161152addac8887
Andrew Davison <andrew.davison@cnrs.fr>
2022-02-09 14:52:43 +0100

    Removed file to simplify merging PR with name clash due to case insensitivity

--- src/globals.js
+++
@@ -1,3 +0,0 @@
-export const jobQueueServer = "https://nmpi-staging.hbpneuromorphic.eu";
-export const quotasServer = "https://quotas-staging.hbpneuromorphic.eu";
-export const hw_options = ["BrainScaleS", "SpiNNaker", "BrainScaleS-2", "BrainScaleS-ESS", "Spikey", "Test"];
