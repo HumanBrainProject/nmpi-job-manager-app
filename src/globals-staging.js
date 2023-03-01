1dde567dd38cc0d41f6b68361f748af6615dc841
Andrew Davison <andrew.davison@cnrs.fr>
2023-03-01 17:27:41 +0100

    adapted to use API v3

--- src/globals-staging.js
+++ src/globals-staging.js
@@ -1,5 +1,13 @@
-export const jobQueueServer = "https://nmpi-staging.hbpneuromorphic.eu";
-export const quotasServer = "https://quotas-staging.hbpneuromorphic.eu";
-export const validationServer = "https://validation-v2.brainsimulation.eu";
-export const authServer = "https://iam.ebrains.eu";  // todo: switch to iam-int
-export const hw_options = ["BrainScaleS", "SpiNNaker", "BrainScaleS-2", "BrainScaleS-ESS", "Spikey", "Test"];
+export const jobQueueServer = "https://nmpi-v3-staging.hbpneuromorphic.eu";
+export const quotasServer = "https://nmpi-v3-staging.hbpneuromorphic.eu";
+export const validationServer = "https://nmpi-v3-staging.brainsimulation.eu";
+export const authServer = "https://iam.ebrains.eu"; // todo: switch to iam-int
+export const corsProxy = "https://corsproxy.hbpneuromorphic.eu";
+export const hw_options = [
+  "BrainScaleS",
+  "SpiNNaker",
+  "BrainScaleS-2",
+  "BrainScaleS-ESS",
+  "Spikey",
+  "Test",
+];
