cd5af025edeadf38c66d38b32d69349620e0787e
Andrew Davison <andrew.davison@cnrs.fr>
2023-11-10 10:38:05 +0100

    Use API v3 in production

--- src/globals-prod.js
+++ src/globals-prod.js
@@ -1,11 +1,14 @@
-export const jobQueueServer = "https://nmpi.hbpneuromorphic.eu";
-export const quotasServer = "https://quotas.hbpneuromorphic.eu";
-export const validationServer = "https://validation.brainsimulation.eu";
+export const jobQueueServer = "https://nmpi-v3.hbpneuromorphic.eu";
+export const quotasServer = "https://nmpi-v3.hbpneuromorphic.eu";
+export const validationServer = "https://nmpi-v3.brainsimulation.eu";
 export const authServer = "https://iam.ebrains.eu";
+export const corsProxy = "https://corsproxy.hbpneuromorphic.eu";
 export const hw_options = [
   "BrainScaleS",
   "SpiNNaker",
   "BrainScaleS-2",
   "BrainScaleS-ESS",
   "Spikey",
+  "Demo",
+  "Test",
 ];
