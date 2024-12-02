110216ebbf8451549c9d6094157892495ba40d4a
Andrew Davison <andrew.davison@cnrs.fr>
2024-12-02 15:22:28 +0100

    change staging deployment to run on port 80, since SSL is provided by kubernetes

--- src/globals-staging.js
+++ src/globals-staging.js
@@ -1,7 +1,7 @@
 export const jobQueueServer = "https://nmpi-v3-staging.hbpneuromorphic.eu";
 export const authServer = "https://iam-int.ebrains.eu";
 export const authClientId = "neuromorphic-remote-access-staging";
-export const driveServer = "https://corsproxy.hbpneuromorphic.eu/https://drive-int.ebrains.eu";
+export const driveServer = "https://corsproxy.apps.tc.humanbrainproject.eu/https://drive-int.ebrains.eu";
 
 export const hw_options = [
   "BrainScaleS",
